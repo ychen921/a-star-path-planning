1b4482d500a4a4e32ef588abd14e4eb0a0821905
ychen921 <steven88669@gmail.com>
2024-04-07 15:28:30 -0400

    Update ROS package

--- project3_ws/build/turtlebot3_project3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ychen921/project3_ws/src/turtlebot3_project3
-# Build directory: /home/ychen921/project3_ws/build/turtlebot3_project3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
